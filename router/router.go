c3f68096dd90914bd07c5082704aa6824add7a00
Guilherme Lemos Vieira <guilhermebomdespacholemos@gmail.com>
2025-03-21 12:49:13 -0500

    feat: cors *

--- router/router.go
+++ router/router.go
@@ -1,9 +1,22 @@
 ﻿package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func Initialize() {
 	r := gin.Default()
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	r.Use(cors.New(config))
 	initializeRoutes(r)
 	err := r.Run(":8080")
 	if err != nil {
